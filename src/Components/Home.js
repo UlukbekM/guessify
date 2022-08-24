07cbac2cdd033d23b78b33cae5f4c86f629b4818
UlukbekM <44413738+UlukbekM@users.noreply.github.com>
2022-08-24 18:12:33 -0400

    test

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,8 +4,8 @@ import { Header } from "./Header";
 //https://discoverquickly.com/
 
 export const CLIENT_ID = 'a36fba8567644702b88f6cab02ecfffa'
-export const REDIRECT_URI = 'http://localhost:3000'
-// export const REDIRECT_URI = 'https://spotify-app-ulukbek.herokuapp.com'
+// export const REDIRECT_URI = 'http://localhost:3000'
+export const REDIRECT_URI = 'https://spotify-app-ulukbek.herokuapp.com'
 export const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize'
 export const RESPONSE_TYPE = 'token'
 export const SCOPE = 'user-top-read streaming user-read-private user-read-email user-modify-playback-state'
