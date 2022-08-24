fb8a9aaa14402e479f797cd1cac6dc477595cb00
UlukbekM <44413738+UlukbekM@users.noreply.github.com>
2022-08-24 18:09:53 -0400

    test

--- src/Components/Testing.js
+++ src/Components/Testing.js
@@ -1,55 +1,55 @@
 import React,{useState, useEffect} from "react";
 import { Header } from "./Header";
-import {fetchData, putData} from './AwsFunctions';
-import AWS from 'aws-sdk'
+// import {fetchData, putData} from './AwsFunctions';
+// import AWS from 'aws-sdk'
 
-const docClient = new AWS.DynamoDB.DocumentClient()
+// const docClient = new AWS.DynamoDB.DocumentClient()
 
 export const Testing = () => {
 
-    const [item, setItem] = useState("")
+    // const [item, setItem] = useState("")
 
-    const fetchDataFromDynamoDb = () => {
-        fetchData('Leaderboard')
-    }
+    // const fetchDataFromDynamoDb = () => {
+    //     fetchData('Leaderboard')
+    // }
 
-    const addDataToDynamoDB = async () => {
-        const userData = {
-            playlistID:"37i9dQZF1DX0XUsuxWHRQd",
-            score: 11,
-            userName: "uluuuuu"
-        }
+    // const addDataToDynamoDB = async () => {
+    //     const userData = {
+    //         playlistID:"37i9dQZF1DX0XUsuxWHRQd",
+    //         score: 11,
+    //         userName: "uluuuuu"
+    //     }
         
-        await putData('Leaderboard' , userData)
-    }
+    //     await putData('Leaderboard' , userData)
+    // }
 
-    useEffect(() => {
-        getData('Leaderboard')
-    }, [])
+    // useEffect(() => {
+    //     getData('Leaderboard')
+    // }, [])
 
-    const getData = (tableName) => {
-        var params = {
-            TableName: tableName
-        }
+    // const getData = (tableName) => {
+    //     var params = {
+    //         TableName: tableName
+    //     }
     
-        docClient.scan(params, function (err, data) {
-            if (!err) {
-                // console.log(data)
-                // return(data)
-                setItem(data)
-            } else {
-                console.log(err)
-            }
-        })
-    }
+    //     docClient.scan(params, function (err, data) {
+    //         if (!err) {
+    //             // console.log(data)
+    //             // return(data)
+    //             setItem(data)
+    //         } else {
+    //             console.log(err)
+    //         }
+    //     })
+    // }
 
-    useEffect(() => {
-        console.log(item)
-    }, [item])
+    // useEffect(() => {
+    //     console.log(item)
+    // }, [item])
 
     return(<>
         <Header/>
-        <button onClick={() => fetchDataFromDynamoDb()}> Fetch </button>
-        <button onClick={() => addDataToDynamoDB()}> Put </button>
+        {/* <button onClick={() => fetchDataFromDynamoDb()}> Fetch </button>
+        <button onClick={() => addDataToDynamoDB()}> Put </button> */}
     </>)
 }
