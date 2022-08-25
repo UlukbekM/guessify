9c13c73f35d72cdb58d8790d2a8e3fbb7d9b3eac
UlukbekM <44413738+UlukbekM@users.noreply.github.com>
2022-08-24 20:53:14 -0400

    tst

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,7 +8,7 @@ export const CLIENT_ID = 'a36fba8567644702b88f6cab02ecfffa'
 export const REDIRECT_URI = 'https://spotify-app-ulukbek.herokuapp.com'
 export const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize'
 export const RESPONSE_TYPE = 'token'
-export const SCOPE = 'user-top-read streaming user-read-private user-read-email user-modify-playback-state'
+export const SCOPE = 'user-top-read user-read-private user-read-email'
 
 export const Home = () => {
 //
